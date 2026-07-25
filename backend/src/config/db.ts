b7dd0438ef146d581ca99dfcac66a4b4ed2bdd35
amangupta20044 <a.mann20044@gmail.com>
2026-07-25 17:09:20 +0530

    feat: update MongoDB connection to use environment variable for URI

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb=async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/project1")
+        await mongoose.connect(process.env.MONGO_URI as string);
         console.log("mongodb connected")
     }catch(error){
         console.log("error in mongodb",error);
